a26480f6282ee5d1f47b2bfdedeb51e3addc92c5
Ayodele Ore <ayodeleo@student.unimelb.edu.au>
2022-11-30 12:46:45 +1100

    Added `make_dnn` to imports.

--- topicflow/__init__.py
+++ topicflow/__init__.py
@@ -3,4 +3,4 @@ from . import nn
 from . import utils
 
 from .data import get_mixture_datasets
-from .nn import NeuralODEFlow, RQSCouplingFlow, TopicFlow
+from .nn import NeuralODEFlow, RQSCouplingFlow, TopicFlow, make_dnn
